916d27f70883bcb25501bcf339969b06744126e4
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2025-02-18 23:16:04 +0600

    feat: confirm order page style updated

--- src/pages/ConfirmOrder/ConfirmOrder.jsx
+++ src/pages/ConfirmOrder/ConfirmOrder.jsx
@@ -20,23 +20,79 @@ export default function ConfirmOrder() {
           Confirm Order
         </h1>
 
-        <div className="flex items-center justify-between gap-6 border-y py-3">
-          <p>
-            <span className="font-semibold">Date:</span> 30/12/2025
-          </p>
-          <p>
-            <span className="font-semibold">Invoice No:</span> #awik125sI4
-          </p>
-        </div>
-
-        <div>
+        <div className="flex justify-between gap-16 border-t py-4">
           <div>
             <p className="font-semibold">Invoiced To:</p>
             <p>Jesse Pinkman</p>
             <p>Dhaka</p>
             <p>Bangladesh</p>
           </div>
+          <div>
+            <p>
+              <span className="font-semibold">Invoice No:</span> #awik125sI4
+            </p>
+            <p>
+              <span className="font-semibold">Date:</span> 30/12/2025
+            </p>
+          </div>
         </div>
+
+        <p>
+          <span className="font-semibold">Software Name:</span> White Label
+          Payroll Software
+        </p>
+
+        <p>
+          <span className="font-semibold">Free Software:</span> Payroll Admin
+        </p>
+
+        <p>
+          <span className="font-semibold">Payment Type:</span> Full Payment
+        </p>
+
+        <p>
+          <span className="font-semibold">Payment Method:</span> Bank Transfer
+        </p>
+
+        <p className="font-semibold">Additional Addons:</p>
+        <table className="w-full border-collapse border border-gray-300 text-lg">
+          <thead>
+            <tr className="bg-gray-100t text-base">
+              <th className="border border-gray-300 px-4 py-2 text-left">
+                Item
+              </th>
+              <th className="border border-gray-300 px-4 py-2 text-left">
+                Price
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border border-gray-300 text-base">
+              <td className="border px-4 py-2">
+                One year hosting for your clients software and other items
+              </td>
+              <td className="border px-4 py-2">€230</td>
+            </tr>
+            <tr className="border border-gray-300 text-base">
+              <td className="border px-4 py-2">
+                Web site design once off payment
+              </td>
+              <td className="border px-4 py-2">€320</td>
+            </tr>
+            <tr className="border border-gray-300 text-base">
+              <td className="border px-4 py-2">
+                Website payment integration with backend automation
+              </td>
+              <td className="border px-4 py-2">€75</td>
+            </tr>
+            <tr className="border border-gray-300 text-base">
+              <td className="border px-4 py-2">
+                Branded application for iOS and Android with maintenance
+              </td>
+              <td className="border px-4 py-2">€750</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
   );
