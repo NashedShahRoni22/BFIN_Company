0dbc9a8a590532423f03c191212e3d40be976492
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2025-05-28 15:25:22 +0600

    feat: hpanel link added in reseller

--- src/pages/Reseller/Overview/Overview.jsx
+++ src/pages/Reseller/Overview/Overview.jsx
@@ -1,5 +1,7 @@
 import SectionContainer from "../../../shared/SectionContainer";
 import overviewImg from "../../../assets/reseller/overview.webp";
+import { Link } from "react-router-dom";
+import { FiUserPlus } from "react-icons/fi";
 
 export default function Overview() {
   return (
@@ -21,6 +23,14 @@ export default function Overview() {
           your own margins. You decide how much you make on each product, giving
           you the flexibility and control to maximize your profits.
         </p>
+        <Link
+          to={"https://hpanel.bfinit.com/distributor/register"}
+          target="_blank"
+          className="mt-6 inline-flex items-center justify-center rounded-lg bg-primary px-8 py-3.5 font-medium text-white transition-all hover:shadow-lg"
+        >
+          <FiUserPlus className="mr-2" />
+          Become a Partner
+        </Link>
       </div>
     </SectionContainer>
   );
