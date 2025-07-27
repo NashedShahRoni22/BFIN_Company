af0c4ef7b47b13c643c6b29b046126d54748d8f6
roufhasan <roufhasan5@gmail.com>
2025-07-27 15:30:07 +0600

    refact: slider image updated

--- src/data/sliderData.js
+++ src/data/sliderData.js
@@ -3,6 +3,11 @@ import img2 from "../assets/home-3/slider-2.png";
 import img3 from "../assets/home-3/slider-3.png";
 import img4 from "../assets/home-3/slider-4.png";
 import img5 from "../assets/home-3/slider-5.png";
+import img6 from "../assets/home-3/slider-6.png";
+import img7 from "../assets/home-3/slider-7.png";
+import img8 from "../assets/home-3/slider-8.png";
+import img9 from "../assets/home-3/slider-9.png";
+import img10 from "../assets/home-3/slider-10.png";
 
 export const sliderData = [
   {
@@ -16,11 +21,6 @@ export const sliderData = [
     img: img2,
   },
   {
-    id: 5,
-    title: "Home",
-    img: img5,
-  },
-  {
     id: 3,
     title: "Home",
     img: img3,
@@ -31,38 +31,43 @@ export const sliderData = [
     img: img4,
   },
   {
+    id: 5,
+    title: "Home",
+    img: img5,
+  },
+  {
     id: 6,
     title: "Home",
-    img: img3,
+    img: img6,
   },
   {
     id: 7,
     title: "Home",
-    img: img2,
+    img: img7,
   },
   {
     id: 8,
     title: "Home",
-    img: img3,
+    img: img8,
   },
   {
     id: 9,
     title: "Home",
+    img: img9,
+  },
+  {
+    id: 10,
+    title: "Home",
+    img: img10,
+  },
+  {
+    id: 11,
+    title: "Home",
     img: img2,
   },
   {
-    id: 2,
+    id: 12,
     title: "Home",
-    img: img3,
-  },
-  {
-    id: 5,
-    title: "Home",
-    img: img2,
-  },
-  {
-    id: 6,
-    title: "Home",
-    img: img3,
+    img: img8,
   },
 ];
